e90df81882c5a4e9cda6e65bfb6acd31083675f6
Daniil Golovats <daniil.golovats@tktk.ee>
2024-04-03 22:41:06 +0300

    add week7 code

---
+++ Week7Functions/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChioce = Console.ReadLine();
+
+if (userChioce == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+
+
+
+static void PrintHello()  //function / method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("See you later, aligator");
+}
